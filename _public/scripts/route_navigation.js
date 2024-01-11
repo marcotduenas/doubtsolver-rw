a1c77149343c961852fd2c4c45e58658ab410b5d
Marco Tulio <marcot.dns@gmail.com>
2024-01-11 14:36:15 -0300

    Adding doubts form, and adding doubts list page
    Now the professionals can check wich doubts they have to answer and the asker can ask their questions

--- _public/scripts/route_navigation.js
+++ _public/scripts/route_navigation.js
@@ -1,3 +1,9 @@
+document.getElementById('redirect').addEventListener('click', function() {
+	const route_name = this.getAttribute('data-route');
+	redirect_to_route(route_name);
+});
+
+
 function redirect_to_route(route_name) {
 	window.location.href = route_name;
 }
